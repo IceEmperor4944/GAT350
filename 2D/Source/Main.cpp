62031b1a3a43cddd042a2659a81664fe076605b4
IceEmperor4944 <macaroni49344@gmail.com>
2024-10-03 22:54:26 -0600

    Added Points, Lines, Rects

--- 2D/Source/Main.cpp
+++ 2D/Source/Main.cpp
@@ -8,9 +8,9 @@
 int main(int argc, char* argv[]) {
     Renderer renderer;
     renderer.Initialize();
-    renderer.CreateWindow("2D", 800, 600);
+    renderer.CreateWindow("2D", 1000, 1000);
 
-    Framebuffer framebuffer(renderer, 100, 75);
+    Framebuffer framebuffer(renderer, 250, 250);
 
     bool quit = false;
     while (!quit) {
@@ -30,12 +30,19 @@ int main(int argc, char* argv[]) {
 
         framebuffer.Clear(color_t{ 0, 0, 0, 255 });
 
-        for (int i = 0; i < 1; i++) {
-            int x = rand() % 100;
-            int y = rand() % 75;
+        for (int i = 0; i < 15; i++) {
+            int x = rand() % 250;
+            int y = rand() % 250;
+            int x2 = rand() % 250;
+            int y2 = rand() % 250;
+            int x3 = rand() % 250;
+            int y3 = rand() % 250;
+            color_t col = { rand() % 255, rand() % 255, rand() % 255, 255 };
             framebuffer.DrawPoint(x, y, { 255, 255, 255, 255 });
+            framebuffer.DrawRect(100, 100, 10, 10, col);
+            //framebuffer.DrawLine(x, y, x2, y2, col);
+            //framebuffer.DrawTriangle(100, 100, 100, 200, 200, 150, col);
         }
-        framebuffer.DrawRect(10, 10, 100, 100, { 255, 255, 255, 255 });
 
         framebuffer.Update();
 
